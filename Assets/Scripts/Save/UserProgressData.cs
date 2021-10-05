bf0b68d4bc05c8b72f2bf5e76c3841fc93dd4304
GandaBagus <81636945+GandaBagus@users.noreply.github.com>
2021-10-05 19:18:29 +0700

    local

---
+++ Assets/Scripts/Save/UserProgressData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+
+
+[System.Serializable]
+
+public class UserProgressData
+
+{
+
+    public double Gold = 0;
+
+    public List<int> resourceLevels = new List<int>();
+
+}
